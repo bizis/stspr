1f0a7b8ddc4d164dc120a7115e5cc9b8c4fac7c9
Alexander Mansurov <alexander.mansurov@gmail.com>
2013-07-12 11:13:01 -0400

    #18 fix

---
+++ StSpr/src/am/bizis/stspr/po/Podnik.java
@@ -0,0 +1,59 @@
+package am.bizis.stspr.po;
+
+import am.bizis.stspr.IPodnik;
+import am.bizis.stspr.OsobaTyp;
+import am.bizis.stspr.fo.Adresa;
+
+public class Podnik implements IPodnik {
+
+	public Podnik() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getIC() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDIC() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getJmeno() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OsobaTyp getTyp() {
+		return OsobaTyp.PO;
+	}
+
+	@Override
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getTelefon() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public int getFax() {
+		return 0;
+	}
+
+	@Override
+	public Adresa getAdresa() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
